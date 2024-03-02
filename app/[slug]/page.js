3cdda2d559b2d0c1aa4adf8b3b3f73541ab345cf
Abhishek Singhal <abhisinghal112001@gmail.com>
2024-03-02 10:10:42 +0530

    intitial commit

---
+++ app/[slug]/page.js
@@ -0,0 +1,20 @@
+"use client"
+import { redirect } from "next/dist/server/api-utils"
+import { useEffect } from "react"
+
+ 
+export default function page({params}) {
+
+  useEffect(() => {
+    if(params.slug >= 1000000000 && params.slug <= 9999999999 )
+      window.location.assign("tel:9927245142")
+    else{
+      alert("Not a valid number please try with another number")
+    }
+  
+    
+  }, [])
+  
+
+  return <p>Post: {params.slug}</p>
+}
